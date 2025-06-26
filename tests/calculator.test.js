420b958c51905f88287f5ed361747315e7147f9d
Vishva Thakkar <vishva@Vishvas-MacBook-Air.local>
2025-06-26 14:29:38 -0400

    TDD-DEMO : STEP 2 :  Return the sum of multiple comma-separated numbers

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -12,4 +12,8 @@ describe('String Calculator', () => {
     expect(add('1,2')).toBe(3);
     expect(add('10,20')).toBe(30);
   });
+  test('returns sum of multiple comma-separated numbers', () => {
+    expect(add('1,2,3')).toBe(6);
+    expect(add('10,20,30')).toBe(60);
+  });
 });
